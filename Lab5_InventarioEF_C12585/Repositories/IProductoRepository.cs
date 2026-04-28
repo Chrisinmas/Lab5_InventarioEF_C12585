e04466ba75c7f0b2c8664ee195ff9d223f9323e1
Christopher <duartebarahonachristopher1@gmail.com>
2026-04-28 16:45:43 -0600

    implementando IProductoRepository y ProductoRepository

---
+++ Lab5_InventarioEF_C12585/Repositories/IProductoRepository.cs
@@ -0,0 +1,14 @@
+﻿using Lab5_InventarioEF.Models;
+using Lab5_InventarioEF_C12585.Models;
+
+namespace Lab5_InventarioEF_C12585.Repositories
+{
+    public interface IProductoRepository
+    {
+        IEnumerable<Producto> ObtenerTodos();
+        Producto ObtenerPorId(int id);
+        void Agregar(Producto p);
+        void Actualizar(Producto p);
+        void Eliminar(int id);
+    }
+}
---
+++ Lab5_InventarioEF_C12585/Repositories/ProductoRepository.cs
@@ -0,0 +1,42 @@
+﻿using Lab5_InventarioEF.Models;
+using Lab5_InventarioEF_C12585.Data;
+using Lab5_InventarioEF_C12585.Models;
+
+namespace Lab5_InventarioEF_C12585.Repositories
+{
+    public class ProductoRepository : IProductoRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ProductoRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Producto> ObtenerTodos() => _context.Productos.ToList();
+
+        public Producto ObtenerPorId(int id) => _context.Productos.Find(id);
+
+        public void Agregar(Producto p)
+        {
+            _context.Productos.Add(p);
+            _context.SaveChanges();
+        }
+
+        public void Actualizar(Producto p)
+        {
+            _context.Productos.Update(p);
+            _context.SaveChanges();
+        }
+
+        public void Eliminar(int id)
+        {
+            var producto = _context.Productos.Find(id);
+            if (producto != null)
+            {
+                _context.Productos.Remove(producto);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
